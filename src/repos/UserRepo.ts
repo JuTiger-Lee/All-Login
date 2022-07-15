f7f98d025559ec8cd887dc2f48dae219a2a663dd
Jeffey <ref6318@gmail.com>
2022-07-16 00:55:05 +0900

    [FIX]: interface implement

--- src/repos/UserRepo.ts
+++ src/repos/UserRepo.ts
@@ -1,8 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 import { Inject, Service } from "typedi";
+import UserRepoable from "@/repos/interfaces/UserRepoable";
 
 @Service()
-export default class UserRepo {
+export default class UserRepo implements UserRepoable {
   constructor(@Inject("Prisma") private readonly prisma: PrismaClient) {
     this.prisma = prisma;
   }
--- src/services/UserServices.ts
+++ src/services/UserServices.ts
@@ -1,7 +1,8 @@
 import UserRepoable from "@/repos/interfaces/UserRepoable";
 import { Inject } from "typedi";
+import UserServicesable from "@/services/interfaces/UserServicesable";
 
-export default class UserServices {
+export default class UserServices implements UserServicesable {
   constructor(@Inject("UserRepo") private readonly userRepo: UserRepoable) {
     this.userRepo = userRepo;
   }
