e908fc8ea73ca20e0cc05069cdb4005c82cbf49c
Jeffey <ref6318@gmail.com>
2022-07-22 01:13:21 +0900

    [FIX]: env 체크 메서드, passport init 메서드 추가

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,38 @@
 import express from "express";
 import routers from "@/routers/index";
+import passport from "passport";
+import env from "env-var";
+import PassportHandler from "@/handler/PassportHandler";
+import dotenv from "dotenv";
 
 class App {
-  public server: express.Application;
-  public PORT: number;
+  public readonly server: express.Application;
+  public readonly PORT: number;
 
   constructor() {
+    this.PORT = env.get("PORT").default(8080).asPortNumber();
     this.server = express();
-    this.PORT = Number(process.env.PORT) || 8080;
-  }
-
-  setRouter() {
-    routers(this.server);
   }
 
   bootstrap() {
+    this.checkEnv();
+    this.initPassport();
     this.setRouter();
+  }
+
+  private checkEnv() {
+    dotenv.config();
+    env.get("DATABASE_URL").required().asString();
+    env.get("AUTH_KEY").required().asString();
+  }
+
+  private initPassport() {
+    this.server.use(passport.initialize());
+    new PassportHandler().init();
+  }
+
+  private setRouter() {
+    routers(this.server);
   }
 }
 
