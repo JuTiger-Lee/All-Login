f033d78297a2eb4d7ac7a8f4bddc7abd0c32e5c8
Jeffey <ref6318@gmail.com>
2022-07-22 01:15:01 +0900

    [FIX]: 코드 정렬

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -2,6 +2,6 @@ import express from "express";
 import ErrorMiddleware from "@/middlewares/error";
 
 export default (app: express.Application) => {
-  app.get("/", (req, res) => res.send("Hello Login"));
+  app.get("/", (req, res) => res.send("Hello All Login"));
   app.use(ErrorMiddleware.serverError);
 };
