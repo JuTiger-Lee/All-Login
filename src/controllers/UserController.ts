9031ac5ed03e71e669297b92c42fa4a747a5e7b8
Jeffey <ref6318@gmail.com>
2022-07-22 01:14:34 +0900

    [FIX]: make-response 파일 변경으로 인한 수정

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,10 +1,37 @@
+import express from "express";
 import UserServicesable from "@/services/interfaces/UserServicesable";
+import { SuccessResponseable } from "@/utils/make-response";
 import { Inject } from "typedi";
 
 export default class UserController {
   constructor(
-    @Inject("UserServices") private readonly userServices: UserServicesable
+    @Inject("UserServices") private readonly userServices: UserServicesable,
+    @Inject("MakeSuccessResponse")
+    private readonly makeSuccessResponse: SuccessResponseable
   ) {
     this.userServices = userServices;
   }
+
+  signUp(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    const { email, password } = req.body;
+
+    try {
+      this.makeSuccessResponse.init(200, "Sign Up Success");
+      this.makeSuccessResponse.setResponse([]);
+
+      return res.json(this.makeSuccessResponse.getResponse());
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  signIn(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {}
 }
--- src/services/UserServices.ts
+++ src/services/UserServices.ts
@@ -1,9 +1,14 @@
 import UserRepoable from "@/repos/interfaces/UserRepoable";
-import { Inject } from "typedi";
 import UserServicesable from "@/services/interfaces/UserServicesable";
+import { MakeErrorResponse } from "@/utils/make-response";
+import { Inject } from "typedi";
 
 export default class UserServices implements UserServicesable {
-  constructor(@Inject("UserRepo") private readonly userRepo: UserRepoable) {
+  constructor(
+    @Inject("UserRepo") private readonly userRepo: UserRepoable,
+    @Inject("MakeErrorResponse")
+    private readonly makeErrorResponse: MakeErrorResponse
+  ) {
     this.userRepo = userRepo;
   }
 }
