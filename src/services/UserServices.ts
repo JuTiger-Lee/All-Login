0fde91383373aca515e056cb7e8d701d7a8ab0cb
Jeffey <ref6318@gmail.com>
2022-07-25 00:54:55 +0900

    [FIX]: method 추가 및 DI, IOC 정책 변경

--- src/services/UserServices.ts
+++ src/services/UserServices.ts
@@ -1,14 +1,37 @@
 import UserRepoable from "@/repos/interfaces/UserRepoable";
 import UserServicesable from "@/services/interfaces/UserServicesable";
-import { MakeErrorResponse } from "@/utils/make-response";
-import { Inject } from "typedi";
+import { ErrorResponseable } from "@/utils/make-response";
+import Container, { Service } from "typedi";
 
+@Service()
 export default class UserServices implements UserServicesable {
-  constructor(
-    @Inject("UserRepo") private readonly userRepo: UserRepoable,
-    @Inject("MakeErrorResponse")
-    private readonly makeErrorResponse: MakeErrorResponse
-  ) {
-    this.userRepo = userRepo;
+  private readonly userRepo: UserRepoable;
+  private readonly makeErrorResponse: ErrorResponseable;
+
+  constructor() {
+    this.userRepo = Container.get("UserRepo");
+    this.makeErrorResponse = Container.get("MakeErrorResponse");
+  }
+
+  async getUser(email: string) {
+    return await this.userRepo.findEmail(email);
+  }
+
+  async checkEmail(email: string) {
+    const user = await this.userRepo.findEmail(email);
+
+    if (user) {
+      this.makeErrorResponse.init(400, "The same email already exists.");
+      this.makeErrorResponse.setResponse({
+        err: {},
+        httpStatus: 400,
+        name: "",
+      });
+      throw this.makeErrorResponse.getResponse();
+    }
+  }
+
+  async saveUser({ email, password }: { email: string; password: string }) {
+    await this.userRepo.createUser({ email, password });
   }
 }
--- src/services/interfaces/UserServicesable.ts
+++ src/services/interfaces/UserServicesable.ts
@@ -1,1 +1,13 @@
-export default interface UserServicesable {}
+import { user } from "@prisma/client";
+
+export default interface UserServicesable {
+  getUser(email: string): Promise<user>;
+  checkEmail(email: string): Promise<void>;
+  saveUser({
+    email,
+    password,
+  }: {
+    email: string;
+    password: string;
+  }): Promise<void>;
+}
