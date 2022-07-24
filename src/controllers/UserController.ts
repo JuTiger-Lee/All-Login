8ad3da9832d5f737396bbf4f45ab57ea5998bfad
Jeffey <ref6318@gmail.com>
2022-07-25 00:56:56 +0900

    [FIX]: testAuth 메서드 추가 및 signIn 메서드 코드 구현

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,31 +1,47 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import env from "env-var";
 import UserServicesable from "@/services/interfaces/UserServicesable";
 import { SuccessResponseable } from "@/utils/make-response";
-import { Inject } from "typedi";
+import Container from "typedi";
+import HashHanlder from "@/handler/HashHandler";
+import Context from "@/Context";
+import passport from "passport";
 
-export default class UserController {
-  constructor(
-    @Inject("UserServices") private readonly userServices: UserServicesable,
-    @Inject("MakeSuccessResponse")
-    private readonly makeSuccessResponse: SuccessResponseable
-  ) {
-    this.userServices = userServices;
+export default class UserController extends Context {
+  private readonly userServices: UserServicesable;
+  private readonly makeSuccessResponse: SuccessResponseable;
+
+  constructor() {
+    super();
+    this.userServices = Container.get("UserServices");
+    this.makeSuccessResponse = Container.get("MakeSuccessResponse");
   }
 
-  signUp(
+  async signUp(
     req: express.Request,
     res: express.Response,
     next: express.NextFunction
   ) {
-    const { email, password } = req.body;
-
     try {
+      const { email, password }: { email: string; password: string } = req.body;
+
+      await this.userServices.checkEmail(email);
+
+      const hashHanlder = new HashHanlder();
+      const hashPassword = hashHanlder.getHash(password);
+
+      this.userServices.saveUser({
+        email,
+        password: hashPassword,
+      });
+
       this.makeSuccessResponse.init(200, "Sign Up Success");
       this.makeSuccessResponse.setResponse([]);
 
-      return res.json(this.makeSuccessResponse.getResponse());
+      return res.status(201).json(this.makeSuccessResponse.getResponse());
     } catch (err) {
-      next(err);
+      return next(err);
     }
   }
 
@@ -33,5 +49,55 @@ export default class UserController {
     req: express.Request,
     res: express.Response,
     next: express.NextFunction
-  ) {}
+  ) {
+    try {
+      passport.authenticate("local", (passportError, user, info) => {
+        if (passportError) {
+          console.log("::PassportError", passportError);
+          // Todo Error
+        }
+
+        if (info) {
+          console.log("::info", info.message);
+          if (info.message === "Non Existent User") {
+            // Todo Error ::존재하지 않는 사용자
+          } else if (info.message === "Wrong Password") {
+            // Todo Error :: 비밀번호 불일치
+          }
+        }
+
+        req.login(user, { session: false }, (loginError) => {
+          if (loginError) {
+            console.log("::loginError", loginError);
+            // Todo Error
+          }
+
+          const token = jwt.sign(
+            {
+              email: user.email,
+            },
+            env.get("AUTH_KEY").asString(),
+            {
+              expiresIn: "3600m",
+            }
+          );
+
+          this.makeSuccessResponse.init(200, "Sign In Success");
+          this.makeSuccessResponse.setResponse([{ token }]);
+        });
+      })(req, res);
+
+      return res.status(200).json(this.makeSuccessResponse.getResponse());
+    } catch (err) {
+      return next(err);
+    }
+  }
+
+  testAuth(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    res.status(200).send("Auth Success");
+  }
 }
