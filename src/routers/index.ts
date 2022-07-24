c7e030da68e96983c2b63eecf2aafb46d91c28bc
Jeffey <ref6318@gmail.com>
2022-07-25 00:54:19 +0900

    [FIX]: Auth middleware 코드 추가

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,7 +1,14 @@
 import express from "express";
+import AuthGuard from "@/middlewares/AuthGuard";
 import ErrorMiddleware from "@/middlewares/error";
+import userRouter from "@/routers/user";
 
 export default (app: express.Application) => {
+  const authGuard = new AuthGuard();
+  const authURList = ["/api/user/auth/test"];
+
+  app.use(authURList, authGuard.checkAuth);
   app.get("/", (req, res) => res.send("Hello All Login"));
+  app.use("/api/user", userRouter);
   app.use(ErrorMiddleware.serverError);
 };
