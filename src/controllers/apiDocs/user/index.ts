085b9a68f21f49ac111776072da5daad56cbabb9
Jeffey <ref6318@gmail.com>
2022-07-31 22:49:52 +0900

    [FIX]: kakao api 추가

--- src/controllers/apiDocs/user/index.ts
+++ src/controllers/apiDocs/user/index.ts
@@ -1,9 +1,11 @@
 import signUp from "@/controllers/apiDocs/user/signUp";
 import signIn from "@/controllers/apiDocs/user/signIn";
 import authTest from "@/controllers/apiDocs/user/authTest";
+import kakao from "@/controllers/apiDocs/user/kakao";
 
 export default {
   ...signUp,
   ...signIn,
   ...authTest,
+  ...kakao,
 };
