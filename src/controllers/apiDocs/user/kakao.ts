ced335afb7b77d18e379e7400c04f8d80b0aa561
Jeffey <ref6318@gmail.com>
2022-07-31 22:45:12 +0900

    [ADD]: kakao api document

---
+++ src/controllers/apiDocs/user/kakao.ts
@@ -0,0 +1,54 @@
+import { PathItem } from "swagger-jsdoc";
+
+export default {
+  // "/api/user/kakao": {
+  //   get: {
+  //     tags: ["User"],
+  //     summary: "카카오 사용자 로그인 (개발 중)",
+  //   },
+  // } as PathItem,
+  "/api/user/kakao/sign-out": {
+    get: {
+      tags: ["User"],
+      summary: "카카오 사용자 로그아웃 (개발 중)",
+      parameters: [
+        {
+          in: "query",
+          name: "accessToken",
+          required: true,
+          description: "kakao access Token",
+          type: "string",
+        },
+      ],
+      responses: {
+        200: {
+          description: "카카오 로그아웃 성공",
+          content: {
+            "application/json": {
+              schema: {
+                type: "object",
+                properties: {
+                  code: {
+                    type: "number",
+                    description: "code",
+                    example: 200,
+                  },
+                  message: {
+                    type: "string",
+                    description: "성공 메시지",
+                    example: "kakao Sign Out Success",
+                  },
+                  data: {
+                    type: "array",
+                    description: "data",
+                    example: [],
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  } as PathItem,
+};
