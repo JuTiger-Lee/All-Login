f8f8d0fbe96158c770a131438bcbf329817e9a3e
Jeffey <ref6318@gmail.com>
2022-08-05 23:19:22 +0900

    [FIX]: auth 미들웨어 함수 간략화

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -6,9 +6,9 @@ import userRouter from "@/routers/user";
 export default (app: express.Application) => {
   const authURList = ["/api/user/auth/test"];
 
-  app.use(authURList, (req, res, next) => {
-    new AuthGuard().checkAuth(req, res, next);
-  });
+  app.use(authURList, (req, res, next) =>
+    new AuthGuard().checkAuth(req, res, next)
+  );
   app.get("/", (req, res) => res.send("Hello All Login"));
   app.use("/api/user", userRouter);
   app.use(ErrorMiddleware.serverError);
