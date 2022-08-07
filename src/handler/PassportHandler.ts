f336dfddee02a730e2a1e0ff4935bd77d6c781fa
Jeffey <ref6318@gmail.com>
2022-08-07 21:41:48 +0900

    [FIX]: facebook google 코드 작성

--- src/handler/PassportHandler.ts
+++ src/handler/PassportHandler.ts
@@ -1,8 +1,10 @@
 import passport from "passport";
-import passportJWT from "passport-jwt";
 import passportLocal from "passport-local";
+import passportJWT from "passport-jwt";
 // passportKakao.Strategy 접근시 not a constructor error 발생
 import * as passportKakao from "passport-kakao";
+import passportFacebook from "passport-facebook";
+import passportGoogle from "passport-google-oauth20";
 import env from "env-var";
 import Container from "typedi";
 import UserServicesable from "@/services/interfaces/UserServicesable";
@@ -10,8 +12,8 @@ import HashHanlder from "@/handler/HashHandler";
 import Context from "@/Context";
 import { PRODUCTION_MODE } from "@/utils/util";
 
-interface VerifyPassport<T> {
-  Strategy: T;
+interface VerifyPassportable<S> {
+  Strategy: S;
   passportName: string;
   verify(userService: UserServicesable): void;
 }
@@ -19,12 +21,24 @@ interface VerifyPassport<T> {
 export default class PassportHandler {
   private userService: UserServicesable;
   private static passportType =
-    passportJWT.Strategy || passportLocal.Strategy || passportKakao.Strategy;
-  private verifys: Array<VerifyPassport<typeof PassportHandler.passportType>>;
+    passportJWT.Strategy ||
+    passportLocal.Strategy ||
+    passportKakao.Strategy ||
+    passportFacebook.Strategy ||
+    passportGoogle.Strategy;
+  private verifys: Array<
+    VerifyPassportable<typeof PassportHandler.passportType>
+  >;
 
   constructor() {
     this.userService = Container.get(Context.USER_SERVICES);
-    this.verifys = [new Jwt(), new Local(), new Kakao()];
+    this.verifys = [
+      new Jwt(),
+      new Local(),
+      new Kakao(),
+      new Facebook(),
+      new Google(),
+    ];
   }
 
   init() {
@@ -41,7 +55,7 @@ export default class PassportHandler {
   }
 }
 
-class Jwt implements VerifyPassport<typeof passportJWT.Strategy> {
+class Jwt implements VerifyPassportable<typeof passportJWT.Strategy> {
   Strategy: typeof passportJWT.Strategy;
   passportName: string;
 
@@ -51,32 +65,33 @@ class Jwt implements VerifyPassport<typeof passportJWT.Strategy> {
   }
 
   verify(userService: UserServicesable): void {
-    const jwtOption = {
-      jwtFromRequest: passportJWT.ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: env.get("AUTH_KEY").asString(),
-      ignoreExpiration: false,
-      passReqToCallback: false,
-    };
-
     passport.use(
       this.passportName,
-      new this.Strategy(jwtOption, async (payload, done) => {
-        const user = await userService.getUser(
-          payload.email,
-          payload.loginType
-        );
+      new this.Strategy(
+        {
+          jwtFromRequest: passportJWT.ExtractJwt.fromAuthHeaderAsBearerToken(),
+          secretOrKey: env.get("AUTH_KEY").asString(),
+          ignoreExpiration: false,
+          passReqToCallback: false,
+        },
+        async (payload, done) => {
+          const user = await userService.getUser(
+            payload.email,
+            payload.loginType
+          );
 
-        if (!user) {
-          return done(null, false, { message: "Unauthorized" });
+          if (!user) {
+            return done(null, false, { message: "Unauthorized" });
+          }
+
+          return done(null, user);
         }
-
-        return done(null, user);
-      })
+      )
     );
   }
 }
 
-class Local implements VerifyPassport<typeof passportLocal.Strategy> {
+class Local implements VerifyPassportable<typeof passportLocal.Strategy> {
   Strategy: typeof passportLocal.Strategy;
   passportName: string;
 
@@ -86,33 +101,34 @@ class Local implements VerifyPassport<typeof passportLocal.Strategy> {
   }
 
   verify(userService: UserServicesable): void {
-    const passportOption = {
-      usernameField: "email",
-      passwordField: "password",
-    };
-
     passport.use(
       this.passportName,
-      new this.Strategy(passportOption, async (email, password, done) => {
-        const user = await userService.getUser(email, "LOCAL");
+      new this.Strategy(
+        {
+          usernameField: "email",
+          passwordField: "password",
+        },
+        async (email, password, done) => {
+          const user = await userService.getUser(email, "LOCAL");
 
-        if (!user) {
-          return done(null, false, { message: "Non Existent User" });
+          if (!user) {
+            return done(null, false, { message: "Non Existent User" });
+          }
+
+          const hashHandler = new HashHanlder();
+
+          if (hashHandler.compare(user.password, password)) {
+            return done(null, false, { message: "Wrong Password" });
+          }
+
+          return done(null, { verifyedUser: user });
         }
-
-        const hashHandler = new HashHanlder();
-
-        if (hashHandler.compare(user.password, password)) {
-          return done(null, false, { message: "Wrong Password" });
-        }
-
-        return done(null, user);
-      })
+      )
     );
   }
 }
 
-class Kakao implements VerifyPassport<typeof passportKakao.Strategy> {
+class Kakao implements VerifyPassportable<typeof passportKakao.Strategy> {
   Strategy: typeof passportKakao.Strategy;
   passportName: string;
 
@@ -128,7 +144,7 @@ class Kakao implements VerifyPassport<typeof passportKakao.Strategy> {
         {
           clientID: env.get("KAKAO_CLIENT_ID").asString(),
           clientSecret: "",
-          callbackURL: "http://localhost:8080/api/user/kakao/callback",
+          callbackURL: "/api/user/kakao/callback",
         },
         async (accessToken, refreshToken, profile, done) => {
           try {
@@ -136,18 +152,18 @@ class Kakao implements VerifyPassport<typeof passportKakao.Strategy> {
             const loginType = "KAKAO";
             const kakaoEmail =
               profile._json && profile._json.kakao_account.email;
-            const kakaoUser = await userService.getUser(kakaoEmail, loginType);
+            const user = await userService.getUser(kakaoEmail, loginType);
 
             const doneData = {
               accessToken,
               refreshToken,
             };
 
-            if (kakaoUser) {
-              return done(null, { ...doneData, kakaoUser: kakaoUser });
+            if (user) {
+              return done(null, { ...doneData, verifyedUser: user });
             }
 
-            const kakaoNewUser = await userService.saveUser({
+            const newUser = await userService.saveUser({
               loginType,
               email: kakaoEmail,
               name: profile.displayName,
@@ -156,7 +172,119 @@ class Kakao implements VerifyPassport<typeof passportKakao.Strategy> {
 
             return done(null, {
               ...doneData,
-              kakaoUser: kakaoNewUser,
+              verifyedUser: newUser,
+            });
+          } catch (err) {
+            return done(err);
+          }
+        }
+      )
+    );
+  }
+}
+
+class Facebook implements VerifyPassportable<typeof passportFacebook.Strategy> {
+  Strategy: typeof passportFacebook.Strategy;
+  passportName: string;
+
+  constructor() {
+    this.Strategy = passportFacebook.Strategy;
+    this.passportName = Kakao.prototype.constructor.name.toLowerCase();
+  }
+
+  verify(userService: UserServicesable): void {
+    passport.use(
+      new this.Strategy(
+        {
+          clientID: env.get("FACEBOOK_CLIENT_ID").asString(),
+          clientSecret: env.get("FACEBOOK_CLIENT_SECRET").asString(),
+          callbackURL: "/api/user/facebook/callback",
+          profileFields: ["id", "email", "displayName"],
+        },
+        async (accessToken, refreshToken, profile, done) => {
+          try {
+            console.log("accessToken =>>", accessToken);
+            const hashHandler = new HashHanlder();
+            const loginType = "FACEBOOK";
+            // 페이스북에서 정상적인 사용자 이메일을 받으려면 http가 아닌 https로 해야한다.
+            const email =
+              (profile.emails && profile.emails[0].value) ||
+              "facebookTest@facebook.com";
+            const user = await userService.getUser(email, loginType);
+
+            const doneData = {
+              accessToken,
+              refreshToken,
+            };
+
+            if (user) {
+              return done(null, { ...doneData, verifyedUser: user });
+            }
+
+            const newUser = await userService.saveUser({
+              loginType,
+              email,
+              name: profile.displayName,
+              password: hashHandler.getHash(hashHandler.getRandomSuffix(5)),
+            });
+
+            return done(null, {
+              ...doneData,
+              verifyedUser: newUser,
+            });
+          } catch (err) {
+            return done(err);
+          }
+        }
+      )
+    );
+  }
+}
+
+class Google implements VerifyPassportable<typeof passportGoogle.Strategy> {
+  Strategy: typeof passportGoogle.Strategy;
+  passportName: string;
+
+  constructor() {
+    this.Strategy = passportGoogle.Strategy;
+    this.passportName = Google.prototype.constructor.name.toLowerCase();
+  }
+
+  verify(userService: UserServicesable): void {
+    passport.use(
+      this.passportName,
+      new this.Strategy(
+        {
+          clientID: env.get("GOOGLE_CLIENT_ID").asString(),
+          clientSecret: env.get("GOOGLE_CLIENT_SECRET").asString(),
+          callbackURL: "/api/user/google/callback",
+        },
+        async (accessToken, refreshToken, profile, done) => {
+          try {
+            const hashHandler = new HashHanlder();
+            const loginType = "GOOGLE";
+            const email = profile._json && profile._json.email;
+            const user = await userService.getUser(email, loginType);
+
+            const doneData = {
+              accessToken,
+              refreshToken,
+            };
+
+            if (user) {
+              return done(null, { ...doneData, verifyedUser: user });
+            }
+
+            const newUser = await userService.saveUser({
+              loginType,
+              email,
+              name: profile.displayName,
+              password: hashHandler.getHash(hashHandler.getRandomSuffix(5)),
+            });
+
+            return done(null, {
+              ...doneData,
+              verifyedUser: newUser,
             });
           } catch (err) {
             return done(err);
