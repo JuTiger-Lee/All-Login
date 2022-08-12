e3530ff953aa1c9162721cf286fdaf3559be5ede
Jeffey <ref6318@gmail.com>
2022-08-12 21:34:09 +0900

    [FIX]: 코드 가독성 업

--- src/routers/user.ts
+++ src/routers/user.ts
@@ -20,36 +20,36 @@ router.post("/auth/test", (req, res, next) =>
 
 // Kakao
 
-router.get("/kakao", (req, res, next) => {
-  userController.userKakao.signIn(req, res, next);
-});
-router.get("/kakao/callback", (req, res, next) => {
-  userController.userKakao.signInCallback(req, res, next);
-});
+router.get("/kakao", (req, res, next) => 
+  userController.userKakao.signIn(req, res, next)
+);
+router.get("/kakao/callback", (req, res, next) => 
+  userController.userKakao.signInCallback(req, res, next)
+);
 router.get("/kakao/sign-out", async (req, res, next) =>
   userController.userKakao.signOut(req, res, next)
 );
 
 // Facebook
 
-router.get("/facebook", (req, res, next) => {
-  userController.userFacebook.signIn(req, res, next);
-});
-router.get("/facebook/callback", (req, res, next) => {
-  userController.userFacebook.signInCallback(req, res, next);
-});
+router.get("/facebook", (req, res, next) => 
+  userController.userFacebook.signIn(req, res, next)
+);
+router.get("/facebook/callback", (req, res, next) => 
+  userController.userFacebook.signInCallback(req, res, next)
+);
 router.get("/facebook/sign-out", async (req, res, next) =>
   userController.userFacebook.signOut(req, res, next)
 );
 
 // google
 
-router.get("/google", (req, res, next) => {
-  userController.userGoogle.signIn(req, res, next);
-});
-router.get("/google/callback", (req, res, next) => {
-  userController.userGoogle.signInCallback(req, res, next);
-});
+router.get("/google", (req, res, next) => 
+  userController.userGoogle.signIn(req, res, next)
+);
+router.get("/google/callback", (req, res, next) => 
+  userController.userGoogle.signInCallback(req, res, next)
+);
 router.get("/google/sign-out", async (req, res, next) =>
   userController.userGoogle.signOut(req, res, next)
 );
